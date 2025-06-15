216e78c3786eb1fd373e5d5de80b3904eb14d4ff
knightmafia <erimar2678@students.ecpi.edu>
2025-06-15 13:21:39 -0500

    End of day 3

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-user_prompt = 'Enter a todo: '
-
 todos = []
 
 while True:
-    todo = input(user_prompt)
-    todos.append(todo)
-    print(todos)
+    user_action = input("Type add or show: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case "add":
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case "show":
+            for item in todos:
+                print(item)
+        case "exit":
+            break
+
+print("Bye")
